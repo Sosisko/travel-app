69ccb5b779cdf7f5c23217b74f6f4391d7e5fc10
Aleksey <svetlampy@mail.ru>
2022-06-10 21:10:04 +0500

    fixed links

---
+++ src/Content/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <div className="content">
+      {/* <img src="https://images.unsplash.com/photo-1542332213-31f87348057f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" /> */}
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/Logo/Logo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Logo.css";
+import logo from "../img/logo.png";
+
+const Logo = () => {
+  return (
+    <>
+      <h1 className="logo_title">
+        <img className="logo_icon" src={logo}></img>TravelGo
+      </h1>
+    </>
+  );
+};
+
+export default Logo;
